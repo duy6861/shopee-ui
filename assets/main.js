71b7289c36f0e02b24e741ac365be5e1ceb98326
duy6861 <duy6861@gmail.com>
2024-06-15 01:12:55 +0700

    commmit2

--- assets/main.js
+++ assets/main.js
@@ -3,20 +3,36 @@ var headerLoginBtn = document.getElementById('header__log-in-btn')
 var modal = document.querySelector('.modal')
 var registerForm = document.querySelector('#register')
 var logInForm = document.querySelector('#log-in')
+var switchLoginBtn = document.getElementById('auth-form__switch-btn-login')
+var switchRegisterBtn = document.getElementById('auth-form__switch-btn-register')
+console.log(switchLoginBtn)
 console.log(window.getComputedStyle(logInForm).display)
 headerRegisterBtn.addEventListener('click', () => {
     modal.style.display = 'flex'
-    registerForm.style.display = 'block'
     logInForm.style.display = 'none'
+    registerForm.style.display = 'block'
 })
 
 headerLoginBtn.addEventListener('click', () => {
     modal.style.display = 'flex'
-    logInForm.style.display = 'block'
     registerForm.style.display = 'none'
+    logInForm.style.display = 'block'
 })
 modal.onclick = (e) => {
     if (e.target.closest(".auth-form__controls-back")) {
         modal.style.display = 'none'
     }
+}
+var modalOverlay = document.querySelector('.modal__overlay')
+modalOverlay.onclick = () => {
+    modal.style.display = 'none'
+}
+switchLoginBtn.onclick = () => {
+    registerForm.style.display = 'none'
+    logInForm.style.display = 'block'
+}
+
+switchRegisterBtn.onclick = () => {
+    logInForm.style.display = 'none'
+    registerForm.style.display = 'block'
 }
